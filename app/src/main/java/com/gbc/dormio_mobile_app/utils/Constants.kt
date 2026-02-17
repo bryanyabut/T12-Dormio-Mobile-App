6c7ad36faa91c1b539df5b9410b40caefb1ae11a
Patrick Solamillo <patricksolamillo@proton.me>
2026-02-17 17:16:00 -0500

    added Constants.kt with API endpoints

---
+++ app/src/main/java/com/gbc/dormio_mobile_app/utils/Constants.kt
@@ -0,0 +1,8 @@
+object Constants {
+    
+    const val API_AUTH_LOGIN = "/api/auth/login"
+    const val API_AUTH_REGISTER = "/api/auth/register"
+    const val API_AUTH_LOGOUT = "/api/auth/logout"
+    const val API_AUTH_VERIFY = "/api/auth/verify"
+    const val API_AUTH_DEVICE_TOKEN = "/api/auth/device-token"
+}
