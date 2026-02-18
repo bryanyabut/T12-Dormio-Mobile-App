51d2a7fbbc45d73a22e3a51dfbbbb5784ac2cde7
Infinitewumbo <millareslepatrick@gmail.com>
2026-02-18 17:52:00 -0500

    define AuthApiService endpoints for login, register, and user profiles

--- app/src/main/java/com/gbc/dormio_mobile_app/network/AuthApiService.kt
+++ app/src/main/java/com/gbc/dormio_mobile_app/network/AuthApiService.kt
@@ -4,9 +4,37 @@ import com.gbc.dormio_mobile_app.model.*
 import com.gbc.dormio_mobile_app.utils.Constants
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AuthApiService {
+
+    @GET("auth/users")
+    suspend fun getAllUsers(): Response<List<UserDto>>
+
+    @GET("auth/profile")
+    suspend fun getCurrentUser(): Response<UserDto>
+
+    @GET("auth/users/{id}")
+    suspend fun getUserById(
+        @Path("id") userId: String
+    ): Response<UserDto>
+
+    @POST("auth/login")
+    suspend fun login(
+        @Body request: LoginRequest
+    ): Response<LoginResponse>
+
+    @POST("auth/register")
+    suspend fun register(
+        @Body request: RegisterRequest
+    ): Response<RegisterResponse>
+
+    @POST("auth/device-token")
+    suspend fun updateDeviceToken(
+        @Body request: DeviceTokenRequest
+    ): Response<DeviceTokenResponse>
 
 
 
