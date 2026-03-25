6f3f5adad76588c7b663aa770f4cca1fa2110ff9
Patrick Solamillo <patricksolamillo@proton.me>
2026-03-25 01:57:41 -0400

    add stub layout and fragments for meals,budgets, and chores
    placeholder fragment and classes for not yet implemented features
    will be replaced in future branches

---
+++ app/src/main/java/com/gbc/dormio_mobile_app/ui/budget/BudgetFragment.kt
@@ -0,0 +1,6 @@
+package com.gbc.dormio_mobile_app.ui.budget
+
+import androidx.fragment.app.Fragment
+import com.gbc.dormio_mobile_app.R
+
+class BudgetFragment : Fragment(R.layout.fragment_budget)
---
+++ app/src/main/java/com/gbc/dormio_mobile_app/ui/meals/MealsFragment.kt
@@ -0,0 +1,6 @@
+package com.gbc.dormio_mobile_app.ui.meals
+
+import androidx.fragment.app.Fragment
+import com.gbc.dormio_mobile_app.R
+
+class MealsFragment : Fragment(R.layout.fragment_meals)
